d04dd696228e8a1f86de4055c47f96101f4b15d8
docteurbadluck <theodeliot01@gmail.com>
2026-04-11 17:32:26 +0200

    improve from feedback

--- pi/srcs/4_drivers/Computer_Driver/internal/_readFramePayload.cpp
+++ pi/srcs/4_drivers/Computer_Driver/internal/_readFramePayload.cpp
@@ -3,6 +3,8 @@
 
 std::string Computer_Driver::_readFramePayload(size_t len)
 {
+	if (len == 0)
+		return "";
 	std::string payload(len, '\0');
 	if (read(_fd, &payload[0], len) != static_cast<ssize_t>(len))
 		return "";
--- pi/srcs/4_drivers/Computer_Driver/internal/_sendHttpPost.cpp
+++ pi/srcs/4_drivers/Computer_Driver/internal/_sendHttpPost.cpp
@@ -1,6 +1,19 @@
 #include "../Computer_Driver.hpp"
 #include <unistd.h>
 
+static bool write_all(int fd, const char *buf, size_t len)
+{
+	size_t sent = 0;
+	while (sent < len)
+	{
+		ssize_t n = write(fd, buf + sent, len - sent);
+		if (n <= 0)
+			return false;
+		sent += static_cast<size_t>(n);
+	}
+	return true;
+}
+
 bool Computer_Driver::_sendHttpPost(int fd, const std::string &data)
 {
 	const std::string header =
@@ -8,7 +21,7 @@ bool Computer_Driver::_sendHttpPost(int fd, const std::string &data)
 		"Host: " + _cfg.host + "\r\n"
 		"Content-Type: application/octet-stream\r\n"
 		"Content-Length: " + std::to_string(data.size()) + "\r\n\r\n";
-	if (write(fd, header.c_str(), header.size()) < 0)
+	if (!write_all(fd, header.c_str(), header.size()))
 		return false;
-	return write(fd, data.c_str(), data.size()) == static_cast<ssize_t>(data.size());
+	return write_all(fd, data.c_str(), data.size());
 }
