c76a2ef219c3fec7fa339214ef421003798ffddb
docteurbadluck <theodeliot01@gmail.com>
2026-04-11 17:37:22 +0200

    fitness

--- pico/motor/srcs/main.cpp
+++ pico/motor/srcs/main.cpp
@@ -8,8 +8,6 @@ int main()
     gpio_set_dir(25, GPIO_OUT);
     while (1)
     {
-        printf("hello world \n");
-        sleep_ms(100);
         gpio_put(25, 1);
         sleep_ms(500);
         gpio_put(25, 0);
